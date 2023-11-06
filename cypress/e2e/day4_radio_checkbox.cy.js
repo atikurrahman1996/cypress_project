4a0859ff88f14c3dc17050291c00639786d3a163
Atikur Rahman <atik.sqa.connexpay@gmail.com>
2023-11-06 10:15:59 +0600

    radio button and checkbox

---
+++ cypress/e2e/day4_radio_checkbox.cy.js
@@ -0,0 +1,42 @@
+describe(" check UI elements", () => {
+  it(" check radio button", () => {
+    cy.visit("https://testautomationpractice.blogspot.com/");
+
+    // check visibility
+
+    cy.get("#male").should("be.visible");
+    cy.get("#female").should("be.visible");
+
+    // check radio button
+
+    cy.get("#male").check().should("be.checked");
+    cy.get("#female").should("not.be.checked");
+
+    cy.get("#female").check().should("be.checked");
+    cy.get("#male").should("not.be.checked");
+
+    // selecting checkbox
+
+    cy.get("#monday").should("be.visible");
+    cy.get("#monday").check().should("be.checked");
+    //cy.get("#monday").uncheck().should("not.be.checked");
+
+    // selecting all the check box -(combination of tag, class, attribute)
+
+    cy.get(".form-check-input[type='checkbox']").check().should("be.checked");
+    cy.get(".form-check-input[type='checkbox']")
+      .uncheck()
+      .should("not.be.checked");
+
+    //Selecting first & last check box
+
+    cy.get(".form-check-input[type='checkbox']")
+      .first()
+      .check()
+      .should("be.checked");
+    cy.get(".form-check-input[type='checkbox']")
+      .last()
+      .check()
+      .should("be.checked");
+  });
+});
