cadd59c9f54d5dfd56df228eeb620c11222efe09
Atikur Rahman <atik.sqa.connexpay@gmail.com>
2023-11-06 09:23:44 +0600

    explicit assertions

---
+++ cypress/e2e/day3_explicit_assertions.cy.js
@@ -0,0 +1,31 @@
+describe("Assertions example", () => {
+  it("Explicit Assertions", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+
+    //Login
+
+    cy.get("input[placeholder='Username']").type("Admin"); //provide value of input box
+    cy.get("input[placeholder='Password']").type("admin123"); //provide value of input box
+    cy.get("button[type='submit']").click(); //provide value of input box
+
+    //compare expected name and actual name
+
+    let expName = "Paul Collings";
+
+    cy.get(".oxd-userdropdown-name").then((x) => {
+      let actName = x.text();
+
+      //BDD Style
+
+      //expect(actName).to.equal(expName);
+      //expect(actName).to.not.equal(expName); // negative assertion
+
+      //TDD Style
+
+      assert.equal(actName, expName);
+      //assert.notEqual(actName, expName);  // negative assertion
+    });
+  });
+});
