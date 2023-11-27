c6d5c59c7d26b050ba2a66ac2f3712a4fa64cf9c
Atikur Rahman <atikur2013rahman@gmail.com>
2023-11-27 10:32:36 +0600

    iframes

---
+++ cypress/e2e/day8_iFrames.cy.js
@@ -0,0 +1,25 @@
+import "cypress-iframe";
+
+describe("handling frames", () => {
+  it("handling iframes ", () => {
+    cy.visit("http://the-internet.herokuapp.com/iframe");
+    cy.frameLoaded("#mce_0_ifr");
+    cy.iframe("#mce_0_ifr").clear().type("Welcome");
+  });
+
+  /*
+  // 2nd approach
+
+  it.only("handling iframes ", () => {
+    cy.visit("http://the-internet.herokuapp.com/iframe");
+    const iframe = cy
+      .get("#mce_0_ifr")
+      .its("0.contentDocument.body")
+      .should("be.visible")
+      .then(cy.wrap);
+    iframe.clear().type("Welcome");
+  });
+  */
+});
+
+//handling ifrmaes using pluging Installation: npm install -D cypress-iframe
