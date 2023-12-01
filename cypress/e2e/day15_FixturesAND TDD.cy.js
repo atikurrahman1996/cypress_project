0196a2369d94d2eda0a4f5d6b8a61c45d7beba96
Atikur Rahman <atikur2013rahman@gmail.com>
2023-12-01 23:03:41 +0600

    fixtures file

--- cypress/e2e/day15_FixturesAND TDD.cy.js
+++ cypress/e2e/day15_FixturesAND TDD.cy.js
@@ -1,7 +1,7 @@
 describe("MyTestSuit", () => {
-  //Direct access without using hooks
-
-  it("Fixtures Demo Test", () => {
+  //Direct access without using fixtures file & hooks
+  /*
+  it("Test without fixtures file", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
@@ -14,4 +14,46 @@ describe("MyTestSuit", () => {
       "Dashboard"
     );
   });
+  */
+
+  //Direct access using fixtures file
+
+  it.skip("Fixtures Test", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+
+    cy.fixture("orangeHRM.json").then((data) => {
+      cy.get("input[placeholder='Username']").type(data.username);
+      cy.get("input[placeholder='Password']").type(data.password);
+      cy.get("button[type='submit']").click();
+      cy.get(
+        ".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module"
+      ).should("have.text", data.expected);
+    });
+  });
+
+  //access using fixtures file and hooks for multiple it blocks
+
+  let userdata; // global variable
+  before(() => {
+    cy.fixture("OrangeHRM.json").then((data) => {
+      userdata = data;
+    });
+  });
+
+  it.only("Fixtures Test", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").type(userdata.username);
+    cy.get("input[placeholder='Password']").type(userdata.password);
+    cy.get("button[type='submit']").click();
+    cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").should(
+      "have.text",
+      userdata.expected
+    );
+  });
 });
+
+// use fixture file we need to create a json file in the fixtures folder after that we can import this file to our test
