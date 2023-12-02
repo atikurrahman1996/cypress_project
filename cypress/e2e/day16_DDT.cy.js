4b2ba60063b9e5b7f9da13cd0c24ffb12f28dc00
Atikur Rahman <atikur2013rahman@gmail.com>
2023-12-02 09:53:45 +0600

    data driven test DDT

---
+++ cypress/e2e/day16_DDT.cy.js
@@ -0,0 +1,31 @@
+describe("MyTestSuit", () => {
+  it("Data Driven Test", () => {
+    cy.fixture("OranageHRM2.json").then((data) => {
+      cy.visit(
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      );
+
+      data.forEach((userdata) => {
+        cy.get("input[placeholder='Username']").type(userdata.username);
+        cy.get("input[placeholder='Password']").type(userdata.password);
+        cy.get("button[type='submit']").click();
+
+        if (userdata.username === "Admin" && userdata.password === "admin123") {
+          cy.get(
+            ".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module"
+          ).should("have.text", userdata.expected);
+
+          cy.get(".oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon").click(); // logout
+          cy.get(
+            "body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > header:nth-child(2) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > ul:nth-child(2) > li:nth-child(4) > a:nth-child(1)"
+          ).click(); // logout
+        } else {
+          cy.get(".oxd-text.oxd-text--p.oxd-alert-content-text").should(
+            "have.text",
+            userdata.expected
+          );
+        }
+      });
+    });
+  });
+});
