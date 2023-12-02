779744ca2e3171849a65fe84cbf70d129a47af00
Atikur Rahman <atikur2013rahman@gmail.com>
2023-12-02 12:17:49 +0600

    screenshoots

---
+++ cypress/e2e/day19_screenshootsANDVideos.cy.js
@@ -0,0 +1,23 @@
+describe("MyTestSuit", () => {
+  // when our test result was failed cypress will take screenshoot autometically
+  // for intentionally taking screenshoot we can use these below method
+
+  it.skip("Captured ScreenShoot and Videos", () => {
+    cy.visit("https://demo.opencart.com/"); //  Lunch URL
+    cy.screenshot("Homepage"); // this will take home page screenshoot
+    cy.wait(5000);
+    cy.get("img[title='Your Store']").screenshot("Logo"); // this will take screenshoot for specific element
+  });
+
+  // automatically captured screenshoot and videos for failure test or error test
+  // negative test
+
+  it("Captured ScreenShoot and Videos", () => {
+    cy.visit("https://demo.opencart.com/"); //  Lunch URL
+    cy.wait(5000);
+    cy.get("li:nth-child(7) a:nth-child(1)").click(); // Click on Camera section
+    cy.get("div[id='content'] h2").should("have.text", "Tablets"); // Validation for Camera, but we put Tablets for  failure testing purose
+  });
+});
+
+// To verify the auto screenshoots and videos for failure test go the terminal > npx cypress run --spec (file path)
